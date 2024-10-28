37a08e6e1096b3cce52afa011de2a8a81652cc49
Rahul <rahul.mymail1@gmail.com>
2024-10-29 04:54:35 +0530

    Add: skills config

--- src/data/config/skill.config.tsx
+++ src/data/config/skill.config.tsx
@@ -1,51 +1,48 @@
 export const SKILLS = [
-    {
-      category: "Frontend",
-      technologies: [
-        "React",
-        "Next.js",
-        "TailwindCSS",
-        "Acternity UI",
-        "ShadCN UI",
-        "Framer motion",
-        "Tanstack Query",
-      ],
-    },
-    {
-      category: "Javascript runtime",
-      technologies: ["Node.js", "Bun", "Edge runtime (Vercel)", "CF Worker"],
-    },
-    {
-      category: "Backend",
-      technologies: ["Hono.js", "Express.js", "Flask", "Next.js"],
-    },
-    {
-      category: "Mobile",
-      technologies: [
-        "React Native",
-        "Expo",
-        "RN MMKV",
-        "RN Reanimated",
-        "RN Gesture Handler",
-      ],
-    },
-    // {
-    //   category: "Web3",
-    //   technologies: [
-    //     "Solana",
-    //     "Anchor",
-    //     "Solana Wallet Provider",
-    //     "Solana Mobile Wallet Provider",
-    //     "Ethereum",
-    //     "Web3.js",
-    //   ],
-    // },
-    {
-      category: "Firmware",
-      technologies: ["C++", "Python", "Micropython"],
-    },
-    {
-      category: "Hardware",
-      technologies: ["ESP32", "ESP8266"],
-    },
-  ];
+  {
+    category: "Frontend",
+    technologies: [
+      "React",
+      "Next.js",
+      "TailwindCSS",
+      "Acternity UI",
+      "ShadCN UI",
+      "Tanstack Query",
+      "Redux",
+      "Recoil",
+    ],
+  },
+  {
+    category: "Javascript runtime",
+    technologies: ["Node.js", "Bun", "Edge runtime (Vercel)", "CF Worker"],
+  },
+  {
+    category: "Backend",
+    technologies: ["Hono.js", "Express.js", "websocket", "gRPC", "Next.js"],
+  },
+  {
+    category: "Database(ORM and ODM)",
+    technologies: ["Prisma", "Mongoose"],
+  },
+  {
+    category: "Mobile",
+    technologies: ["React Native", "Expo"],
+  },
+  {
+    category: "Firmware",
+    technologies: ["C++", "Python", "Java"],
+  },
+  {
+    category: "Extra",
+    technologies: [
+      "Redis",
+      "Prometheus and grafana",
+      "vitest",
+      "docker",
+      "k8s",
+      "proto-buf",
+      "Kafka",
+      "CICD",
+    ],
+  },
+];
